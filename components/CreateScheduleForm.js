0999388da4d398d34b46d7b7a98e1efc2652e861
Luke Sanger <lukesngr@gmail.com>
2023-09-01 23:20:01 +1000

    Making new schedules form

--- components/CreateScheduleForm.js
+++ components/CreateScheduleForm.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
+
 export default function CreateScheduleForm() {
     const [name, setName] = useState("");
     const [boxSizeNumber, setBoxSizeNumber] = useState(0);
     const [boxSizeUnit, setBoxSizeUnit] = useState("min");
     const [endDate, setEndDate] = useState("");
-    const [endDateType, setEndDateType] = useState("");
+    const [endDateType, setEndDateType] = useState(false);
     const [wakeupTime, setWakeupTime] = useState("07:30");
 
     function handleSubmit(event) {
@@ -22,11 +23,11 @@ export default function CreateScheduleForm() {
                 <option value="hr">Hour</option>
             </select><br />
             <label>End Date: </label>
-            <select value={endDateType} onChange={(e) => setEndDateType(e.target.value)}>
-                <option value={true}>None</option>
-                <option value={false}>Choose</option>
+            <select value={endDateType} onChange={(e) => {setEndDateType(e.target.value)}}>
+                <option value={"none"}>None</option>
+                <option value={"initial"}>Choose</option>
             </select>
-            {endDateType && <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} required></input>}
+            <input type="date" style={{display: endDateType} }value={endDate} onChange={(e) => setEndDate(e.target.value)} required></input>
             <br />
             <label>Average Wakeup Time: </label>
             <input type="time" value={wakeupTime} onChange={(e) => setWakeupTime(e.target.value)} required></input> <br />
--- pages/myschedules.js
+++ pages/myschedules.js
@@ -5,16 +5,26 @@ import axios from "axios";
 import NoSchedules from "@/components/NoSchedules";
 import RedirWhenNotAuth from "@/components/RedirWhenNotAuth";
 
-export default function MySchedules() {
-    const {data: session, status} = useSession();
-    const {isLoading, isError, data, error, refetch} = useQuery(["schedules"], () => axios.post("/api/getSchedules", {userEmail: session.user.email}))
+function MySchedulesSeperatedForFunctionality(props) {
+    
+    const {isLoading, isError, data, error, refetch} = useQuery(["schedules"], () => axios.post("/api/getSchedules", {userEmail: props.session.user.email}))
 
     return (
         <>
-            <RedirWhenNotAuth redirectSrc="/signin" status={status}>
-                <SignedInNav session={session}></SignedInNav>
-                {data && <NoSchedules session={session}/>}
+            <RedirWhenNotAuth redirectSrc="/signin" status={props.status}>
+                <SignedInNav session={props.session}></SignedInNav>
+                {data && <NoSchedules session={props.session}/>}
             </RedirWhenNotAuth>
         </>
     )
+}
+
+export default function MySchedules() {
+    const {data: session, status} = useSession();
+
+    if(status == "loading") {
+        return <h1>Loading</h1>
+    }else if(status === "authenticated"){
+        return <MySchedulesSeperatedForFunctionality session={session} status={status}></MySchedulesSeperatedForFunctionality>
+    }  
 }
