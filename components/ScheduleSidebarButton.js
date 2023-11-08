303ada464e858f20f3c1eb000f4d9d3d3bc2378d
Luke Sanger <lukesngr@gmail.com>
2023-11-08 20:36:31 +1100

    Fixing some common issues with the code in sidebar

---
+++ components/ScheduleSidebarButton.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import CreateAreaModal from './CreateAreaModal';
+import { useState } from 'react';
+
+export default function ScheduleSidebarButton(props) {
+    const [isAddAreaVisible, setIsAddAreaVisible] = useState(false);
+
+    function toggleAddAreaButton() {
+        setIsAddAreaVisible(!isAddAreaVisible);
+    }
+
+    return (
+        <div key={props.schedule.id}  onClick={() => props.selectSchedule(props.index)} className={props.selectedSchedule == props.index ? 'selectedSchedule' : 'schedule'}>
+            {props.schedule.name}
+            {!isAddAreaVisible && <FontAwesomeIcon onClick={toggleAddAreaButton} className='scheduleButton' icon={faChevronDown}/> }
+            {isAddAreaVisible && <FontAwesomeIcon onClick={toggleAddAreaButton} className='scheduleButton' icon={faChevronUp}/> }
+            <FontAwesomeIcon className='scheduleButton' icon={faGear} />
+            {isAddAreaVisible && props.schedule.areas.map(area => (<div key={area.id} className="areaButton">{area.name}</div>))}
+            {isAddAreaVisible && <>
+                <button type="button" className="btn btn-dark createButton" data-bs-toggle="modal" data-bs-target="#createAreaModal">
+                    Add area 
+                </button>
+                <CreateAreaModal id={props.schedule.id}/>
+            </>}
+        </div>)
+}
--- components/SchedulesSidebar.js
+++ components/SchedulesSidebar.js
@@ -1,18 +1,13 @@
 import '../styles/schedulessidebar.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGear, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
 import CreateScheduleModal from './CreateScheduleModal';
-import CreateAreaModal from './CreateAreaModal';
 import { useContext, useState } from 'react';
 import { ScheduleContext } from './ScheduleContext';
+import ScheduleSidebarButton from './ScheduleSidebarButton';
 
 export default function SchedulesSidebar(props) {
-    const [isAddAreaVisible, setIsAddAreaVisible] = useState(false);
     const {selectedSchedule, setSelectedSchedule} = useContext(ScheduleContext);
-
-    function toggleAddAreaButton() {
-        setIsAddAreaVisible(!isAddAreaVisible);
-    }
+    
 
     function selectSchedule(id) {
         setSelectedSchedule(id);
@@ -21,19 +16,7 @@ export default function SchedulesSidebar(props) {
     return (
     <div id="schedulesSidebar">
         <h1 className="sidebarHeading">My Schedules</h1>
-        {props.data.data.map((schedule, index) => (<div key={schedule.id}  onClick={() => selectSchedule(index)} className={selectedSchedule == index ? 'selectedSchedule' : 'schedule'}>
-            {schedule.name}
-            {!isAddAreaVisible && <FontAwesomeIcon onClick={toggleAddAreaButton} className='scheduleButton' icon={faChevronDown}/> }
-            {isAddAreaVisible && <FontAwesomeIcon onClick={toggleAddAreaButton} className='scheduleButton' icon={faChevronUp}/> }
-            <FontAwesomeIcon className='scheduleButton' icon={faGear} />
-            {isAddAreaVisible && schedule.areas.map(area => (<div key={area.id} className="areaButton">{area.name}</div>))}
-            {isAddAreaVisible && <>
-                <button type="button" className="btn btn-dark createButton" data-bs-toggle="modal" data-bs-target="#createAreaModal">
-                    Add area 
-                </button>
-                <CreateAreaModal id={schedule.id}/>
-            </>}
-        </div>))}
+        {props.data.data.map((schedule, index) => (<ScheduleSidebarButton index={index} selectedSchedule={selectedSchedule} schedule={schedule} selectSchedule={selectSchedule}></ScheduleSidebarButton>))}
         <button type="button" className="btn btn-dark createButton" data-bs-toggle="modal" data-bs-target="#createFirstScheduleModal">
             Add schedule 
         </button>
