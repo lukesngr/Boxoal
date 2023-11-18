c2fec7c3fe5f1c779ff5520caa03b095d82b23ca
Luke Sanger <lukesngr@gmail.com>
2023-11-18 16:58:59 +1100

    Fixing common errors

--- pages/api/createTimebox.js
+++ pages/api/createTimebox.js
@@ -1,9 +1,15 @@
 import prisma from "@/modules/prismaClient";
 
 export default async function handler(req, res) {
-    const data = req.body;
-    await prisma.timeBox.create({
-        data: data
-    })
-    res.status(200)
-}
+    try {
+      const data = req.body;
+      await prisma.timeBox.create({
+        data: data,
+      });
+      res.status(200).json({ message: 'TimeBox created successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
