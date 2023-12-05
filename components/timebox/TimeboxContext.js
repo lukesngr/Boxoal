02ed0f07a8b1903e90994a89e906a7be8d7b7876
Luke Sanger <lukesngr@gmail.com>
2023-12-05 13:13:04 +1100

    added recording variable to context

--- components/timebox/TimeboxContext.js
+++ components/timebox/TimeboxContext.js
@@ -5,9 +5,10 @@ const TimeboxContext = createContext();
 function TimeboxContextProvider({ children }) {
     const [timeBoxInUse, setTimeBoxInUse] = useState("");
     const listOfColors = ["#00E3DD", "#00C5E6", "#00A4E7", "#0081DC", "#1E5ABF", "#348D9D", "#67D6FF"];
+    const [timeboxRecording, setTimeBoxRecording] = useState(-1);
     
     return (
-      <TimeboxContext.Provider value={{ timeBoxInUse, setTimeBoxInUse, listOfColors }}>
+      <TimeboxContext.Provider value={{ timeBoxInUse, setTimeBoxInUse, listOfColors, timeboxRecording, setTimeBoxRecording }}>
         {children}
       </TimeboxContext.Provider>
     );
