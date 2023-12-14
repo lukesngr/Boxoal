6be9eab11f09309c3e60e88d50d61005719c4777
Luke Sanger <lukesngr@gmail.com>
2023-12-15 08:02:34 +1100

    Trying to fix bugs with recorded timeboxes

--- components/timebox/RecordedTimeBoxOverlay.js
+++ components/timebox/RecordedTimeBoxOverlay.js
@@ -1,25 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { calculatePixelsFromTopOfGridBasedOnTime } from "@/modules/dateLogic";
 
 export default function RecordedTimeBoxOverlay(props) {
     let {data, schedule, overlayDimensions} = props;
-    let recordedBoxes = [];
+    const [recordedBoxes, setRecordedBoxes] = useState([]);
 
     useEffect(() => {
         if(data.length > 0) {
+            let normalArrayFromState = recordedBoxes;
             data.forEach(element => {
                 let marginFromTop = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedStartTime));
                 let heightForBox = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedEndTime)) - marginFromTop;
-                console.log(heightForBox);
-                recordedBoxes.push({heightForBox, marginFromTop, title: element.timeBox.title});
+                if(data.some(item => item.marginFromTop != marginFromTop &&
+                    item.heightForBox != heightForBox && item.title != element.timeBox.title)) {
+                    normalArrayFromState.push({heightForBox, marginFromTop, title: element.timeBox.title});
+                }
             });
+            setRecordedBoxes(normalArrayFromState);
         }
     }, [data]);
 
-
+    console.log(recordedBoxes);
     
     return <>{recordedBoxes.map((recordedBoxes) => (
         <div className="recordedTimeBox" style={{width: props.overlayDimensions[0]+"px", 
-        height: recordedBoxes.heightForBox, top: marginFromTop}}></div>
+        height: recordedBoxes.heightForBox, top: recordedBoxes.marginFromTop}}></div>
     ))}</>
 }
--- components/timebox/RecordingOverlay.js
+++ components/timebox/RecordingOverlay.js
@@ -20,9 +20,9 @@ export default function RecordingOverlay(props) {
         }
     }, [timeboxRecording])
 
-    if(isNaN(props.activeOverlayHeight)) {
+    /*if(isNaN(props.activeOverlayHeight)) {
         console.log(props.activeOverlayHeight+' is NaN');
-    }
+    }*/
 
     return (
         <>
