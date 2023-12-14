6759b3b76b44f99e901b221fc4ec4d17f71a6ebb
Luke Sanger <lukesngr@gmail.com>
2023-12-14 19:07:21 +1100

    TRying to get all the formatting working cuz

--- components/timebox/RecordedTimeBoxOverlay.js
+++ components/timebox/RecordedTimeBoxOverlay.js
@@ -3,23 +3,23 @@ import { calculatePixelsFromTopOfGridBasedOnTime } from "@/modules/dateLogic";
 
 export default function RecordedTimeBoxOverlay(props) {
     let {data, schedule, overlayDimensions} = props;
+    let recordedBoxes = [];
 
     useEffect(() => {
-        let recordedBoxes = [];
-        let marginFromTop;
-
         if(data.length > 0) {
             data.forEach(element => {
-                marginFromTop = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedStartTime));
+                let marginFromTop = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedStartTime));
                 let heightForBox = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedEndTime)) - marginFromTop;
                 console.log(heightForBox);
                 recordedBoxes.push({heightForBox, marginFromTop, title: element.timeBox.title});
             });
         }
-        console.log(recordedBoxes);
     }, [data]);
 
 
     
-    return <></>
+    return <>{recordedBoxes.map((recordedBoxes) => (
+        <div className="recordedTimeBox" style={{width: props.overlayDimensions[0]+"px", 
+        height: recordedBoxes.heightForBox, top: marginFromTop}}></div>
+    ))}</>
 }
