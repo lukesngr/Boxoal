2f97f1c6c8f00dac009f6fb220a6aab47e52c0cc
Luke Sanger <lukesngr@gmail.com>
2023-12-19 18:17:17 +1100

    Realized this a quick fix to fundamental problem in application

--- components/timebox/RecordedTimeBoxOverlay.js
+++ components/timebox/RecordedTimeBoxOverlay.js
@@ -11,8 +11,9 @@ export default function RecordedTimeBoxOverlay(props) {
             data.forEach(element => {
                 let marginFromTop = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedStartTime));
                 let heightForBox = calculatePixelsFromTopOfGridBasedOnTime(schedule, overlayDimensions, new Date(element.recordedEndTime)) - marginFromTop;
+                let notEitherZero = !(marginFromTop == 0 || heightForBox == 0); //due to overlay dimensions not being set at right time
 
-                if(!normalArrayFromState.some(item => item.id === element.id)) {
+                if(notEitherZero && !normalArrayFromState.some(item => item.id === element.id)) {
                     normalArrayFromState.push({id: element.id, heightForBox, marginFromTop, title: element.timeBox.title});
                 }
             });
