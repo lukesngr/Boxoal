aec3e5482b72bb8853abacf94e57ffc50aeb4bf0
Luke Sanger <lukesngr@gmail.com>
2023-12-20 21:30:30 +1100

    making so refetch anywhere

--- pages/myschedules.js
+++ pages/myschedules.js
@@ -6,23 +6,22 @@ import NoSchedules from "@/components/schedule/NoSchedules";
 import RedirWhenNotAuth from "@/components/RedirWhenNotAuth";
 import { createContext } from "react";
 import SchedulesView from "@/components/schedule/SchedulesView";
+import { RefetchContextProvider } from "@/components/base/RefetchContext";
 
 export const SessionContext = createContext();
 
 function MySchedulesSeperatedForFunctionality(props) {
     
     const {status, data, error, refetch} = useQuery(["schedules"], () => axios.post("/api/getSchedules", {userEmail: props.session.user.email}))
-    
-    if(status === 'success') {
-        data.refetch = refetch;
-    }
 
     return (
         <>
             <RedirWhenNotAuth redirectSrc="/signin" status={props.status}>
                 <SignedInNav session={props.session}></SignedInNav>
                 <SessionContext.Provider value={props.session}>
+                    <RefetchContext.Provider value={refetch}>
                     {data && data.data.length > 0 ? (<SchedulesView data={data}></SchedulesView>) : (<NoSchedules session={props.session}/>) }
+                    </RefetchContext.Provider>
                 </SessionContext.Provider>
             </RedirWhenNotAuth>
         </>
