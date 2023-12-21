d44d0cebd1e0cd9c1e474f7c174cc458c48545dd
Luke Sanger <lukesngr@gmail.com>
2023-12-21 20:24:24 +1100

    Added it now gotta listen for window changes

--- components/schedule/SchedulesSidebar.js
+++ components/schedule/SchedulesSidebar.js
@@ -4,10 +4,16 @@ import CreateScheduleModal from './CreateScheduleModal';
 import { useContext, useState } from 'react';
 import { ScheduleContext } from './ScheduleContext';
 import ScheduleSidebarButton from './ScheduleSidebarButton';
+import { useMediaQuery } from 'react-responsive';
 
 export default function SchedulesSidebar(props) {
     const {selectedSchedule, setSelectedSchedule} = useContext(ScheduleContext);
     const [expanded, setExpanded] = useState(true);
+    const smallerThanLargeBreakpoint = useMediaQuery({query: '(max-width: 992px)'});
+
+    if(smallerThanLargeBreakpoint) {
+        setExpanded(false);
+    }
     
 
     function selectSchedule(id) {
@@ -23,7 +29,7 @@ export default function SchedulesSidebar(props) {
     }
 
     return (
-        <div className={"col-2 schedulesSidebarContainer "+getCorrectClass()}>
+        <div className={"col-2 schedulesSidebarContainer"} style={{'display': expanded ? ('none') : ('block')}}>
             <div className="schedulesSidebar">
                 <h1 className="sidebarHeading">My Schedules</h1>
                 {props.data.data.map((schedule, index) => (<ScheduleSidebarButton key={index} index={index} selectedSchedule={selectedSchedule} schedule={schedule} selectSchedule={selectSchedule}></ScheduleSidebarButton>))}
