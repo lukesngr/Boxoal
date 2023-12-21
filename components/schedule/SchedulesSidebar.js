173c548f50a0da67bc4764801d043afbd7d07ca2
Luke Sanger <lukesngr@gmail.com>
2023-12-22 09:25:39 +1100

    Implemented minimize button now adding expand for the sidebar

--- components/schedule/SchedulesSidebar.js
+++ components/schedule/SchedulesSidebar.js
@@ -1,37 +1,34 @@
 import '../../styles/schedulessidebar.scss';
 
 import CreateScheduleModal from './CreateScheduleModal';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ScheduleContext } from './ScheduleContext';
 import ScheduleSidebarButton from './ScheduleSidebarButton';
 import { useMediaQuery } from 'react-responsive';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight, faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
 
 export default function SchedulesSidebar(props) {
     const {selectedSchedule, setSelectedSchedule} = useContext(ScheduleContext);
     const [expanded, setExpanded] = useState(true);
-    const smallerThanLargeBreakpoint = useMediaQuery({query: '(max-width: 992px)'});
 
-    if(smallerThanLargeBreakpoint) {
-        setExpanded(false);
-    }
+    let smallerThanLargeBreakpoint = useMediaQuery({query: '(max-width: 992px)'});
+
+    useEffect(() => {
+        setExpanded(!smallerThanLargeBreakpoint);
+        console.log(expanded);
+    }, [smallerThanLargeBreakpoint])
+    
     
 
     function selectSchedule(id) {
         setSelectedSchedule(id);
     }
 
-    function getCorrectClass() {
-        if(expanded) {
-            return 'expandedSideBar'
-        }else{
-            return 'closedSideBar'
-        }
-    }
-
     return (
-        <div className={"col-2 schedulesSidebarContainer"} style={{'display': expanded ? ('none') : ('block')}}>
+        <div className={"col-2 schedulesSidebarContainer"} style={{'display': expanded ? ('block') : ('none')}}>
             <div className="schedulesSidebar">
-                <h1 className="sidebarHeading">My Schedules</h1>
+                <h1 className="sidebarHeading">My Schedules <FontAwesomeIcon onClick={() => setExpanded(false)} className='minimizeButton' icon={faArrowLeft}></FontAwesomeIcon></h1>
                 {props.data.data.map((schedule, index) => (<ScheduleSidebarButton key={index} index={index} selectedSchedule={selectedSchedule} schedule={schedule} selectSchedule={selectSchedule}></ScheduleSidebarButton>))}
                 <button type="button" className="btn btn-dark createButton" data-bs-toggle="modal" data-bs-target="#createFirstScheduleModal">
                     Add schedule 
