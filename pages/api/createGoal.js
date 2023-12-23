8098fdf6d3d8af1cdc8523c9b03e143d4d359bff
Luke Sanger <lukesngr@gmail.com>
2023-12-23 18:29:49 +1100

    Changed structure

--- pages/api/createGoal.js
+++ pages/api/createGoal.js
@@ -1,9 +1,15 @@
 import prisma from "@/modules/prismaClient";
 
 export default async function handler(req, res) {
-    const data = req.body;
-    await prisma.goal.create({
-        data: data
-    })
-    res.status(200)
+    try {
+        const data = req.body;
+        await prisma.goal.create({
+            data: data
+        })
+        res.status(200).json({ message: 'Created goal successfully' });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      } finally {
+        await prisma.$disconnect();
+      }
 }
