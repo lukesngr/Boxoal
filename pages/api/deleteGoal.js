51a7cbd76298054bf7d112f5649f8573180bf380
Luke Sanger <lukesngr@gmail.com>
2023-12-27 15:07:50 +1100

    api route done now adding button

---
+++ pages/api/deleteGoal.js
@@ -0,0 +1,25 @@
+import prisma from "@/modules/prismaClient";
+
+export default async function handler(req, res) {
+    try {
+      const data = req.body;
+
+      await prisma.goal.delete({
+        where: {
+          id: data.id
+        },
+        include: {
+          timeboxes: true,
+          recordedTimeboxes: true,
+        },
+      });
+
+      res.status(200).json({ message: 'Goal deleted successfully' });
+    } catch (error) {
+      console.error(error);
+  
+      res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
