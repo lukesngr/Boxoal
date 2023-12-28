e5809cd877711d277b19d968e3e8d0fd4b9b5cfc
Luke Sanger <lukesngr@gmail.com>
2023-12-28 19:32:43 +1100

    Fixed error with portal component

--- components/base/PortalComponent.js
+++ components/base/PortalComponent.js
@@ -1,8 +1,14 @@
-import {  useEffect } from "react";
+import {  useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 export default function PortalComponent(props) {
-    const portalRoot = document.getElementById('portalRoot');
-      
-    return createPortal(props.children, portalRoot);
+
+    const [portal, setPortal] = useState(props.children);
+
+    useEffect(() => {
+        const portalRoot = document.getElementById('portalRoot');
+        setPortal(createPortal(props.children, portalRoot));
+    }, [props.children])
+
+    return portal;
 }
