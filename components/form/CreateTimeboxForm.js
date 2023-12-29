d0915dc9155bdab5fc0abcca23c13f4a79263189
Luke Sanger <lukesngr@gmail.com>
2023-12-29 14:05:50 +1100

    almost finished moving form to component

---
+++ components/form/CreateTimeboxForm.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+export default function CreateTimeboxForm(props) {
+    let [numberOfBoxes, setNumberOfBoxes] = props.numberOfBoxes;
+    let initialSelectedGoal;
+
+    if(schedule.goals.length > 0) {
+        initialSelectedGoal = schedule.goals[0].id;
+    }else{
+        initialSelectedGoal = null;
+    }
+
+    const [goalSelected, setGoalSelected] = useState(initialSelectedGoal);
+    const [timeBoxFormVisible, setTimeBoxFormVisible] = useState(false);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    function closeTimeBox() {
+        setTimeBoxFormVisible(false);
+        setAddTimeBoxDialogOpen(false);
+    }
+    
+
+    let maxNumberOfBoxes = calculateMaxNumberOfBoxes(schedule, time, date);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let startTime = convertToDateTime(time, date);
+        let endTime = convertToDateTime(addBoxesToTime(schedule.boxSizeUnit, schedule.boxSizeNumber, time, numberOfBoxes), date); //add boxes to start time to get end time
+        let color = listOfColors[Math.floor(Math.random() * listOfColors.length)]; //randomly pick a box color
+
+        if(goalSelected === null) { toast.error("No goal selected, please select or make one"); }
+
+        //post to api
+        axios.post('/api/createTimebox', 
+            {title, description, startTime, endTime, numberOfBoxes: parseInt(numberOfBoxes), color, schedule: {connect: {id: schedule.id}}, goal: {connect: {id: goalSelected}}}
+        ).then(() => {
+            //reset the form
+            queryClient.refetchQueries();
+            setAddTimeBoxDialogOpen(false);
+            setTimeBoxFormVisible(false);
+            setTitle("");
+            setDescription("");
+            setNumberOfBoxes(1);
+            toast.success("Added timebox!", {
+                position: toast.POSITION.TOP_RIGHT,
+            });
+        }).catch(function(error) {
+            toast.error("Error occurred please try again or contact developer");
+            console.log(error); 
+        })
+    }
+
+    return <>
+        {timeBoxFormVisible && <div id={dayName == 'Sat' ? 'addTimeBoxConstrained' : 'addTimeBox'}> 
+            <div id="timeBoxBubble"></div>
+            <button onClick={closeTimeBox} id="addTimeBoxExitButton">X</button>
+            <form onSubmit={handleSubmit}>
+                <h4>Add TimeBox</h4>
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input><br />
+                <label htmlFor="description">Description</label>
+                <input type="text" name="description" id="description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></input><br />
+                <label htmlFor="boxes">Boxes</label>
+                <input min="1" max={maxNumberOfBoxes} type="number" name="boxes" id="boxes" placeholder="Boxes" value={numberOfBoxes} onChange={(e) => setNumberOfBoxes(e.target.value)}></input><br />
+                <label>Goal: </label>
+                <select value={goalSelected} onChange={(e) => {console.log(e.target.value); setGoalSelected(e.target.value)}}>
+                    {schedule.goals.map((goal) => (
+                        <option value={String(goal.id)}>{goal.name}</option>
+                    ))}
+                </select>
+                <button id="addTimeBoxButton">Add TimeBox</button>
+            </form>
+        </div>}
+    </>
+}
--- components/timebox/Timebox.js
+++ components/timebox/Timebox.js
@@ -8,40 +8,21 @@ import axios from 'axios';
 import { TimeboxContext } from './TimeboxContext';
 import {toast} from "react-toastify";
 import { queryClient } from './../../pages/_app';
+import CreateTimeboxForm from '../form/CreateTimeboxForm';
 
 export default function TimeBox(props) {
 
     const {schedule, time, date, active, dayName, data, overlayFuncs} = props;
     const [pauseActiveOverlay, resumeActiveOverlay] = overlayFuncs;
-    const [timeBoxFormVisible, setTimeBoxFormVisible] = useState(false);
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [numberOfBoxes, setNumberOfBoxes] = useState(1);
     const [recordedStartTime, setRecordedStartTime] = useState();
+    const [numberOfBoxes, setNumberOfBoxes] = useState(1);
     const {addTimeBoxDialogOpen, setAddTimeBoxDialogOpen, listOfColors, timeboxRecording, setTimeBoxRecording} = useContext(TimeboxContext);
-
-    let initialSelectedGoal;
-
-    if(schedule.goals.length > 0) {
-        initialSelectedGoal = schedule.goals[0].id;
-    }else{
-        initialSelectedGoal = null;
-    }
-
-    const [goalSelected, setGoalSelected] = useState(initialSelectedGoal);
-
-    let maxNumberOfBoxes = calculateMaxNumberOfBoxes(schedule, time, date);
 
     function addTimeBox() {
         if(!addTimeBoxDialogOpen) {
             setTimeBoxFormVisible(true);
             setAddTimeBoxDialogOpen(true);
         }
-    }
-
-    function closeTimeBox() {
-        setTimeBoxFormVisible(false);
-        setAddTimeBoxDialogOpen(false);
     }
 
     function getHeightForBoxes(numberOfBoxes) { return `calc(${(numberOfBoxes * 100)}% + ${(numberOfBoxes - 1) * 2}px)` }
@@ -70,60 +51,10 @@ export default function TimeBox(props) {
         })  
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        let startTime = convertToDateTime(time, date);
-        let endTime = convertToDateTime(addBoxesToTime(schedule.boxSizeUnit, schedule.boxSizeNumber, time, numberOfBoxes), date); //add boxes to start time to get end time
-        let color = listOfColors[Math.floor(Math.random() * listOfColors.length)]; //randomly pick a box color
-
-        if(goalSelected === null) {
-            toast.error("No goal selected, please select or make one");
-        }
-
-        //post to api
-        axios.post('/api/createTimebox', 
-            {title, description, startTime, endTime,
-                numberOfBoxes: parseInt(numberOfBoxes), color, schedule: {connect: {id: schedule.id}}, goal: {connect: {id: goalSelected}}}
-        ).then(() => {
-            //reset the form
-            queryClient.refetchQueries();
-            setAddTimeBoxDialogOpen(false);
-            setTimeBoxFormVisible(false);
-            setTitle("");
-            setDescription("");
-            setNumberOfBoxes(1);
-            toast.success("Added timebox!", {
-                position: toast.POSITION.TOP_RIGHT,
-            });
-        }).catch(function(error) {
-            toast.error("Error occurred please try again or contact developer");
-            console.log(error); 
-        })
-    }
-
     return (
     <div className={'col timeBox'}>
         {/* Form section of this TimeBox component */}
-        {timeBoxFormVisible && <div id={dayName == 'Sat' ? 'addTimeBoxConstrained' : 'addTimeBox'}> 
-            <div id="timeBoxBubble"></div>
-            <button onClick={closeTimeBox} id="addTimeBoxExitButton">X</button>
-            <form onSubmit={handleSubmit}>
-                <h4>Add TimeBox</h4>
-                <label htmlFor="title">Title</label>
-                <input type="text" name="title" id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input><br />
-                <label htmlFor="description">Description</label>
-                <input type="text" name="description" id="description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></input><br />
-                <label htmlFor="boxes">Boxes</label>
-                <input min="1" max={maxNumberOfBoxes} type="number" name="boxes" id="boxes" placeholder="Boxes" value={numberOfBoxes} onChange={(e) => setNumberOfBoxes(e.target.value)}></input><br />
-                <label>Goal: </label>
-                <select value={goalSelected} onChange={(e) => {console.log(e.target.value); setGoalSelected(e.target.value)}}>
-                    {schedule.goals.map((goal) => (
-                        <option value={String(goal.id)}>{goal.name}</option>
-                    ))}
-                </select>
-                <button id="addTimeBoxButton">Add TimeBox</button>
-            </form>
-        </div>}
+        <CreateTimeboxForm schedule={schedule} time={time} date={date} numberOfBoxes={[numberOfBoxes, setNumberOfBoxes]}></CreateTimeboxForm>
 
         {/* Normal time box */}
         {data && <div style={{height: getHeightForBoxes(data.numberOfBoxes), backgroundColor: data.color}} id="timeBox">
