5aa3cb8c3a37362e1f42fb38078962456ed21a02
Luke Sanger <lukesngr@gmail.com>
2023-12-30 16:01:44 +1100

    Update works

--- components/modal/UpdateGoalModal.js
+++ components/modal/UpdateGoalModal.js
@@ -3,7 +3,7 @@ import BootstrapModal from "./BootstrapModal";
 import '../../styles/createmodal.scss';
 
 export default function UpdateGoalModal(props) {
-    return <BootstrapModal render={props.render} id="updateGoalModal" title="Update Goal">
+    return <BootstrapModal render={props.render} id={"updateGoalModal"+props.goal.id} title="Update Goal">
             <UpdateGoalForm goal={props.goal}></UpdateGoalForm>
     </BootstrapModal>
 }
--- components/modal/UpdateScheduleModal.js
+++ components/modal/UpdateScheduleModal.js
@@ -3,7 +3,7 @@ import BootstrapModal from "./BootstrapModal";
 import '../../styles/createmodal.scss';
 
 export default function UpdateScheduleModal(props) {
-    return <BootstrapModal render={props.render} id="updateScheduleModal" title="Update Schedule">
+    return <BootstrapModal render={props.render} id={"updateScheduleModal"+props.schedule.id} title="Update Schedule">
                 <UpdateScheduleForm schedule={props.schedule}></UpdateScheduleForm>
             </BootstrapModal>
 }
