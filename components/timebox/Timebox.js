f22a4ff5efa6c9c12cfbd5cb7b42dc83903a97ad
Luke Sanger <lukesngr@gmail.com>
2024-01-15 19:46:57 +1100

    Daily reoccurring is done

--- components/timebox/Timebox.js
+++ components/timebox/Timebox.js
@@ -36,13 +36,13 @@ export default function TimeBox(props) {
     }
 
     function startRecording() {
-        setTimeBoxRecording(data.id);
+        setTimeBoxRecording([data.id, date]);
         pauseActiveOverlay();
         setRecordedStartTime(new Date());
     }
 
     function stopRecording() {
-        setTimeBoxRecording(-1);
+        setTimeBoxRecording([-1, 0]);
         resumeActiveOverlay();
         axios.post('/api/createRecordedTimebox', 
             {recordedStartTime, recordedEndTime: new Date(), timeBox: {connect: {id: data.id}}, schedule: {connect: {id: schedule.id}}
@@ -69,9 +69,10 @@ export default function TimeBox(props) {
         {data && <UpdateTimeBoxModal render={tags => (<div style={{height: getHeightForBoxes(data.numberOfBoxes), backgroundColor: data.color}} id="timeBox">
             
             <span {...tags} style={{height: getHeightForBoxes(data.numberOfBoxes)}} className="timeboxText">{data.title}</span>
-
-            {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording == -1 && <button className="recordTimeButton" onClick={startRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleDot} /></button>}
-            {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording == data.id && <button className="stopRecordTimeButton" onClick={stopRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleStop} /></button>}
+            {console.log(timeboxRecording, [data.id, date])}
+            {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording[0] == -1 && <button className="recordTimeButton" onClick={startRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleDot} /></button>}
+            {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording[0] == data.id && timeboxRecording[1] == date 
+            && <button className="stopRecordTimeButton" onClick={stopRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleStop} /></button>}
 
         </div>)} timebox={data}></UpdateTimeBoxModal> }
 
--- components/timebox/TimeboxContext.js
+++ components/timebox/TimeboxContext.js
@@ -5,7 +5,7 @@ const TimeboxContext = createContext();
 function TimeboxContextProvider({ children }) {
     const [addTimeBoxDialogOpen, setAddTimeBoxDialogOpen] = useState(false);
     const listOfColors = ["#00E3DD", "#00C5E6", "#00A4E7", "#0081DC", "#1E5ABF", "#348D9D", "#67D6FF"];
-    const [timeboxRecording, setTimeBoxRecording] = useState(-1);
+    const [timeboxRecording, setTimeBoxRecording] = useState([-1, 0]);
     
     return (
       <TimeboxContext.Provider value={{ addTimeBoxDialogOpen, setAddTimeBoxDialogOpen, listOfColors, timeboxRecording, setTimeBoxRecording }}>
