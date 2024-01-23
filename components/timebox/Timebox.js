8211d96d760cc1f31678b61607409cfb9a2c26d4
Luke Sanger <lukesngr@gmail.com>
2024-01-24 08:32:05 +1100

    Tests are leading to more ideas for refactoring

--- components/timebox/Timebox.js
+++ components/timebox/Timebox.js
@@ -71,15 +71,15 @@ export default function TimeBox(props) {
         <div style={{height: getHeightForBoxes(numberOfBoxes)}} id="placeholderTimeBox">{title}</div></>}
 
         {/* Normal time box */}
-        {data && <UpdateTimeBoxModal render={tags => (<div style={{height: getHeightForBoxes(data.numberOfBoxes), backgroundColor: data.color}} id="timeBox">
-            
-            <span {...tags} style={{height: getHeightForBoxes(data.numberOfBoxes)}} className="timeboxText">{data.title}</span>
-            {console.log(timeboxRecording, [data.id, date])}
-            {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording[0] == -1 && <button className="recordTimeButton" onClick={startRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleDot} /></button>}
-            {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording[0] == data.id && timeboxRecording[1] == date 
-            && <button className="stopRecordTimeButton" onClick={stopRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleStop} /></button>}
+        {data && <UpdateTimeBoxModal render={tags => 
+            (<div style={{height: getHeightForBoxes(data.numberOfBoxes), backgroundColor: data.color}} id="timeBox" date-test>    
+                <span {...tags} style={{height: getHeightForBoxes(data.numberOfBoxes)}} className="timeboxText">{data.title}</span>
+                {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording[0] == -1 && <button className="recordTimeButton" onClick={startRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleDot} /></button>}
+                {isRecordingButtonPresent(data.recordedTimeBoxes, data.reoccuring, date, time) && timeboxRecording[0] == data.id && timeboxRecording[1] == date 
+                && <button className="stopRecordTimeButton" onClick={stopRecording} ><FontAwesomeIcon height={20} width={20} icon={faCircleStop} /></button>}
 
-        </div>)} timebox={data}></UpdateTimeBoxModal> }
+            </div>)
+        } timebox={data}></UpdateTimeBoxModal> }
 
     </div>)
 }
