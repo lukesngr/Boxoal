971f596ae74730df3bd5e1914b0592097cf3504f
Luke Sanger <lukesngr@gmail.com>
2024-01-24 17:15:26 +1100

    Gonna cease writing the test for this and write test for normal timebox

--- tests/components/Timebox.test.js
+++ tests/components/Timebox.test.js
@@ -3,6 +3,7 @@ import { render, fireEvent, act, screen } from '@testing-library/react';
 import axios from 'axios';
 import TimeBox from '@/components/timebox/Timebox';
 import { TimeboxContextProvider } from '@/components/timebox/TimeboxContext';
+import '@testing-library/jest-dom'
 
 jest.mock('axios', () => ({
     post: jest.fn(() => Promise.resolve({ data: {}})),
@@ -13,26 +14,48 @@ beforeEach(() => {
 });
 
 describe('TimeBox component', () => {
-  const mockProps = {
-    schedule: { id: 1, wakeupTime: '07:00', boxSizeNumber: 1, boxSizeUnit: 'hour', goals: [], timeboxes: [], recordedTimeboxes: []},
-    time: '12:00',
-    date: '2024-01-23',
-    active: true,
-    dayName: 'Monday',
-    data: undefined/*{
-      id: 1,
-      numberOfBoxes: 1,
-      color: '#123456',
-      title: 'Test Time Box',
-      recordedTimeBoxes: [],
-      reoccuring: false,
-    }*/,
-    overlayFuncs: [jest.fn(), jest.fn()],
-  };
 
   //there's three different states of a timebox, gonna test them all now
 
   test('transitions to opening timebox form when add timebox clicked', () => {
+    const mockProps = {
+      schedule: { id: 1, wakeupTime: '07:00', boxSizeNumber: 1, boxSizeUnit: 'hour', goals: [], timeboxes: [], recordedTimeboxes: []},
+      time: '12:00', date: '2024-01-23', active: true, dayName: 'Monday',
+      data: undefined,
+      overlayFuncs: [jest.fn(), jest.fn()],
+    };
+
+    /*{
+        id: 1,
+        numberOfBoxes: 1,
+        color: '#123456',
+        title: 'Test Time Box',
+        recordedTimeBoxes: [],
+        reoccuring: false,
+      }*/
+    render(<TimeboxContextProvider><div id="portalRoot"></div><TimeBox {...mockProps} /></TimeboxContextProvider>);
+
+    act(() => {fireEvent.click(screen.getByTestId('addTimeBoxButton'));});
+
+    expect(screen.getByTestId('createTimeboxForm')).toBeInTheDocument();
+    
+  });
+
+  test('when timebox has data, timebox pops up', () => {
+    const mockProps = {
+      schedule: { id: 1, wakeupTime: '07:00', boxSizeNumber: 1, boxSizeUnit: 'hour', goals: [], timeboxes: [], recordedTimeboxes: []},
+      time: '12:00', date: '2024-01-23', active: true, dayName: 'Monday',
+      data: {
+        id: 1,
+        numberOfBoxes: 1,
+        color: '#123456',
+        title: 'Test Time Box',
+        recordedTimeBoxes: [],
+        reoccuring: false,
+      },
+      overlayFuncs: [jest.fn(), jest.fn()],
+    };
+
     render(<TimeboxContextProvider><div id="portalRoot"></div><TimeBox {...mockProps} /></TimeboxContextProvider>);
 
     act(() => {fireEvent.click(screen.getByTestId('addTimeBoxButton'));});
