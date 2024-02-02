8641650ea3d11d2a985d1ab3b2ed71d11ed9cd42
Luke Sanger <lukesngr@gmail.com>
2024-02-02 16:18:49 +1100

    Made loading image a priority

--- components/base/Loading.js
+++ components/base/Loading.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Loading() {
   return (
     <div className="loading">
-        <Image src="/icon2.png" alt="loading" width={150} height={150} />
+        <Image src="/icon2.png" alt="loading" width={150} height={150} priority/>
         <p>Loading... </p>
     </div>
   );
