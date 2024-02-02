df7490d0a0fb155349f40c26d34025cce4f2410b
Luke Sanger <lukesngr@gmail.com>
2024-02-02 12:43:11 +1100

    Font looking good now adding animations

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import SignedInNav from "@/components/nav/SignedInNav";
 import Image from "next/image"
 import "../styles/homecard.scss";
 import { useSession } from "next-auth/react";
+import localFont from 'next/font/local'
+const glitchFont = localFont({src: '../public/BlueScreen.ttf'});
 
 export default function Home() {
 
     const {data: session, status} = useSession();
+    
 
     if(status === "loading") {
         return <p>Loading</p>
@@ -16,12 +19,11 @@ export default function Home() {
     <>
         {!session && <UnSignedNav /> }
         {status === "authenticated" && <SignedInNav session={session}/>}
-        <div className="card mx-auto w-25 mt-3" id="homeCard">
-            <Image className="card-image-top mx-auto" src="/icon.png" width={300} height={290}></Image>
-            <div className="card-body">
-                <h3 className="card-title">Welcome</h3>
-                <p className="card-text">BoxAlc is simple timeboxing software that allows you to organize your day around activities that progress areas of your life. You can record these activites as they are being committed and this improves your ovverall level in the application, allowing you to finetune your estimation of your days</p>
-            </div>
+        <div className="text-center animatedText">
+            <p className={glitchFont.className}>Timeboxing</p>
+            <p className={glitchFont.className}>For</p>
+            <p className={glitchFont.className}>The</p>
+            <p className={glitchFont.className}>Everyman</p>
         </div>
     </>)
 }
