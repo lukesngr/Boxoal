a8012d57dbbbe0fd0195a9d0a8e8cd2731d31bfa
Luke Sanger <lukesngr@gmail.com>
2024-02-02 14:09:45 +1100

    Rotation added now just gona add steps

--- components/base/Loading.js
+++ components/base/Loading.js
@@ -1,9 +1,11 @@
 import '../../styles/loading.scss';
+import Image from 'next/image';
 
 export function Loading() {
   return (
     <div className="loading">
-      <p>Loading... </p>
+        <Image src="/icon2.png" alt="loading" width={150} height={150} />
+        <p>Loading... </p>
     </div>
   );
 }
