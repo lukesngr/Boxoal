6318ad46c93db1aab67e5ae8dcc502ac0e9709b9
Luke Sanger <lukesngr@gmail.com>
2024-03-25 19:17:55 +1100

    Dumb mistake but custom hook works

--- components/timebox/TimeboxRecordingContext.js
+++ components/timebox/TimeboxRecordingContext.js
@@ -1,12 +1,12 @@
 import { createContext, useState } from "react";
 
-const TimeboxRecordingContext = createContext();
+const TimeboxRecordingContext = createContext(null);
 
 function TimeboxRecordingContextProvider({ children }) {
     const [timeboxRecording, setTimeBoxRecording] = useState([-1, 0]);
     
     return (
-      <TimeboxRecordingContext.Provider value={{ timeboxRecording, setTimeBoxRecording }}>
+      <TimeboxRecordingContext.Provider value={[ timeboxRecording, setTimeBoxRecording ]}>
         {children}
       </TimeboxRecordingContext.Provider>
     );
---
+++ hooks/useOverlayDimensions.js
@@ -0,0 +1,36 @@
+export default function useOverlayDimensions() {
+    const { gridContainerRef, headerContainerRef, timeboxColumnRef, setOverlayDimensions, selectedSchedule, expanded } = props;
+
+    function calculateOverlayDimensions() {
+        if (gridContainerRef.current && headerContainerRef.current && timeboxColumnRef.current) { //if ref working
+            const gridHeight = gridContainerRef.current.offsetHeight; //get height of grid
+            const headerHeight = headerContainerRef.current.offsetHeight; //get height of headers
+
+            const headerWidth = headerContainerRef.current.offsetWidth; //get width of headers
+            const overlayHeight = gridHeight - headerHeight; //overlay is under headers but goes till end of grid
+            const timeboxHeight = timeboxColumnRef.current.getBoundingClientRect().height; //decimal for a bit more accuracy as this for active overlay
+
+            setOverlayDimensions([headerWidth, overlayHeight, timeboxHeight]);
+        }
+    };
+
+    //when page first loads calculate overlay dimensions
+    useEffect(() => {
+        calculateOverlayDimensions();
+        window.addEventListener('resize', calculateOverlayDimensions); //if resized calculate overlay dimensions
+    
+        return () => {
+            window.removeEventListener('resize', calculateOverlayDimensions);
+        };
+    }, []);
+
+    //if schedule changes recalculate overlay dimensions
+    useEffect(() => {
+        calculateOverlayDimensions();
+    }, [selectedSchedule]);
+    //if sidebar changes recalculate overlay dimensions
+    useEffect(() => {
+        setOverlayDimensions([0, 0, 0]);
+        setTimeout(calculateOverlayDimensions, 600);
+    }, [expanded]);
+}
