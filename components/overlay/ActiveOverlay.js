cf050524dd5cfbefe93ef4bb2c85e9a7a25700c4
Luke Sanger <lukesngr@gmail.com>
2024-03-26 15:56:40 +1100

    Code ain't working

--- components/overlay/ActiveOverlay.js
+++ components/overlay/ActiveOverlay.js
@@ -2,5 +2,6 @@ import { useSelector } from "react-redux";
 export default function ActiveOverlay() {
     const overlayDimensions = useSelector(state => state.overlayDimensions.value);
     const activeOverlayHeight = useSelector(state => state.activeOverlayHeight.value);
+    console.log(activeOverlayHeight);
     return <div className="overlay" style={{width: overlayDimensions[0]+"px", height: activeOverlayHeight+"px"}}></div>
 }
