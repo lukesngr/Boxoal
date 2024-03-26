bde5719411104c7e76323525d63eda04be8c660f
Luke Sanger <lukesngr@gmail.com>
2024-03-26 20:22:38 +1100

    Think i got this bad boy working

--- redux/activeOverlayInterval.js
+++ redux/activeOverlayInterval.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { useRef } from 'react';
+import { calculateOverlayHeightForNow } from '@/modules/coreLogic';
 
 
   export const activeOverlayInterval = createSlice({
@@ -14,22 +15,25 @@ import { useRef } from 'react';
     },
   })
 
-  export const setTimer = () => (arg, {dispatch, getState}) => {
-    const overlayDimensions = getState().overlayDimensions.value;
-    const {wakeupTime, boxSizeUnit, boxSizeNumber} = getState().scheduleEssentials.value;
-    const newInterval = useRef(null);
-    newInterval.current = setInterval(() => 
-        { 
-            dispatch({type:"activeOverlayHeight/set", payload: calculateOverlayHeightForNow(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions)});
-        }
-    , 5000);
-    dispatch({type: 'activeOverlayInterval/set', payload: newInterval});
+  export function setTimer() { 
+    return function (dispatch, getState) {
+      const overlayDimensions = getState().overlayDimensions.value;
+      const {wakeupTime, boxSizeUnit, boxSizeNumber} = getState().scheduleEssentials.value;
+      const newInterval = setInterval(() => 
+          { 
+              dispatch({type:"activeOverlayHeight/set", payload: calculateOverlayHeightForNow(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions)});
+          }
+      , 5000);
+      dispatch({type: 'activeOverlayInterval/set', payload: newInterval});
+  }
 };
 
-export const resetTimer = (arg, {dispatch, getState}) => {
-    if(getState().activeOverlayInterval.value.current) {
-      clearInterval(getState().activeOverlayInterval.value.current);
+export function resetTimer() {
+   return function(dispatch, getState) {
+    if(getState().activeOverlayInterval.value) {
+      clearInterval(getState().activeOverlayInterval.value);
     }
+   }
 };
 
 // Action creators are generated for each case reducer function
