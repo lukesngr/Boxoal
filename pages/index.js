4b2118cc3b8597d2650009104ecd0430a84480d7
Luke Sanger <lukesngr@gmail.com>
2025-02-22 08:38:59 +1100

    abstracted alert component to top level of function

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,26 @@ import SignInCard  from "@/components/login/SignInCard";
 import CreateAccountCard from "@/components/login/CreateAccountCard";
 import ForgotPasswordCard from "@/components/login/ForgotPasswordCard";
 import LandingPage from "@/components/LandingPage";
+import { Dialog, DialogTitle, DialogContent, DialogContentText } from "@mui/material";
 
 export default function Home() {
 
     const [componentDisplayed, setComponentDisplayed] = useState("landing");
+    const [alert, setAlert] = useState({open: false, title: "", message: ""});
 
     return (
     <>
-        {componentDisplayed == "landing" && <LandingPage setComponentDisplayed={setComponentDisplayed} />}
-        {componentDisplayed == "signIn" && <SignInCard setComponentDisplayed={setComponentDisplayed} />}
-        {componentDisplayed == "createAccount" && <CreateAccountCard setComponentDisplayed={setComponentDisplayed} />}
-        {componentDisplayed == "forgotPassword" && <ForgotPasswordCard setComponentDisplayed={setComponentDisplayed} />}
+        <Dialog open={alert.open} onClose={() => setAlert({open: false, title: "", message: ""})}>
+            <DialogTitle>{alert.title}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                {alert.message}
+                </DialogContentText>
+            </DialogContent>
+        </Dialog>
+        {componentDisplayed == "landing" && <LandingPage setAlert={setAlert} setComponentDisplayed={setComponentDisplayed} />}
+        {componentDisplayed == "signIn" && <SignInCard setAlert={setAlert} setComponentDisplayed={setComponentDisplayed} />}
+        {componentDisplayed == "createAccount" && <CreateAccountCard setAlert={setAlert} setComponentDisplayed={setComponentDisplayed} />}
+        {componentDisplayed == "forgotPassword" && <ForgotPasswordCard setAlert={setAlert} setComponentDisplayed={setComponentDisplayed} />}
     </>)
 }
