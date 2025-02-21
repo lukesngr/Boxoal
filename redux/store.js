664a9a671c2ecd8dc7899b13c8abe448f6514a4f
Luke Sanger <lukesngr@gmail.com>
2025-02-22 08:20:05 +1100

    store works like normal

--- redux/store.js
+++ redux/store.js
@@ -8,20 +8,11 @@ import timeboxGridReducer from './timeboxGrid'
 import timeboxRecordingReducer from './timeboxRecording'
 import timeboxDialogReducer from './timeboxDialog'
 import selectedDateReducer from './selectedDate'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { persistStore, persistReducer } from 'redux-persist';
 import { combineReducers } from '@reduxjs/toolkit'
 import onDayViewReducer from './onDayView'
 import daySelectedReducer from './daySelected'
 import modalVisibleReducer from './modalVisible'
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
+
 
 const rootReducer = combineReducers({
   profile: profileReducer,
@@ -38,22 +29,8 @@ const rootReducer = combineReducers({
   modalVisible: modalVisibleReducer,
 });
 
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  whitelist: ['timeboxRecording', 'activeOverlayInterval', 'activeOverlayHeight', 'overlayDimensions', 'daySelected', 'profile']
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+    getDefaultMiddleware(),
 });
-
-export const persistor = persistStore(store);
