e2e3422ae0d7daea0806f628119306f5addab363
Luke Sanger <lukesngr@gmail.com>
2025-02-21 21:29:55 +1100

    destructured object to make code cleaner

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,10 +1,11 @@
 const glitchFont = localFont({src: '../public/BlueScreen.ttf'});
 
 export default function LandingPage(props) {
+    let {setComponentDisplayed} = props;
     return (
     <div className="text-center animatedText">
         <h1 className={glitchFont.className}>Timeboxing For The Everyman</h1>
-        <button className="signInButton" onClick={() => props.setComponentDisplayed('signIn')}>Join Us</button>
+        <button className="signInButton" onClick={() => setComponentDisplayed('signIn')}>Join Us</button>
     </div>
     )
 }
