7387bce2a9a3f632f6e75e04419c00decf6ea929
Luke Sanger <lukesngr@gmail.com>
2025-02-23 19:10:41 +1100

    put in selected date and date picker working now

--- components/timebox/TimeboxHeading.js
+++ components/timebox/TimeboxHeading.js
@@ -7,36 +7,42 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { ThemeProvider, createTheme } from "@mui/material";
 import "../../styles/timeboxheading.scss";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function TimeboxHeading(props) {
 
-    const theme = createTheme({
-        palette: {
-          mode: 'dark',
-          primary: {
-            main: '#7FFFD4',
-          },
-          secondary: {
-            main: '#e21919',
-          },
-          text: {
-            hint: '#ffffff',
-            secondary: '#ffffff',
-          },
-        }
-      });
+  const dispatch = useDispatch();
+  const theme = createTheme({
+      palette: {
+        mode: 'dark',
+        primary: {
+          main: '#7FFFD4',
+        },
+        secondary: {
+          main: '#e21919',
+        },
+        text: {
+          hint: '#ffffff',
+          secondary: '#ffffff',
+        },
+      }
+    });
 
     const [datePickerVisible, setDatePickerVisible] = useState(false);
-    const {expanded, setExpanded, selectedDate, setSelectedDate} = useContext(ScheduleContext);
+    const selectedDate = useSelector(state => state.selectedDate.value);
+    const expanded = useSelector(state => state.expanded.value);
 
-    return <h1 className="viewHeading">This Week
+    return <h1 className="viewHeading">Timeboxes
                 <FontAwesomeIcon className="calendarIcon" icon={faCalendar} onClick={() => setDatePickerVisible(!datePickerVisible)}></FontAwesomeIcon>
                 {datePickerVisible && <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <ThemeProvider theme={theme}>
                         <StaticDatePicker sx={{position: "absolute", zIndex: 999, left: '50%'}} displayStaticWrapperAs="desktop"
-                        openTo="day" value={selectedDate} onChange={(newValue) => {setSelectedDate(newValue);}}></StaticDatePicker>
+                        openTo="day" value={selectedDate} onChange={(newValue) => {
+                          dispatch({type: 'selectedDate/set', payload: newValue.toUTCString()})
+                          setDatePickerVisible(false);
+                        }}></StaticDatePicker>
                     </ThemeProvider>
                 </LocalizationProvider>}
-                {!expanded && <FontAwesomeIcon className="sideBarExpandBtn ml-1" icon={faCog} onClick={() => setExpanded(true)}></FontAwesomeIcon>}
+                {!expanded && <FontAwesomeIcon className="sideBarExpandBtn ml-1" icon={faCog} onClick={() => {}}></FontAwesomeIcon>}
             </h1>
 }
