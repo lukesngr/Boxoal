54dc0e54e7e66211a11ca90b4f0e1e57370eb2c7
Luke Sanger <lukesngr@gmail.com>
2025-02-24 18:05:59 +1100

    had to do this to stop triggering of hooks changed place during rerender

--- components/timebox/GridHeader.js
+++ components/timebox/GridHeader.js
@@ -2,14 +2,22 @@ import Overlay from '../overlay/Overlay';
 import ActiveOverlay from '../overlay/ActiveOverlay';
 import RecordingOverlay from '../overlay/RecordingOverlay';
 import RecordedTimeBoxOverlay from './RecordedTimeBoxOverlay';
+import { useSelector } from 'react-redux';
 import { ifCurrentDay, ifEqualOrBeyondCurrentDay } from '../../modules/untestableFunctions';
 export default function GridHeader({headerContainerRef, dayToName}) {
+    const onDayView = useSelector(state => state.onDayView.value);
+    const daySelected = useSelector(state => state.daySelected.value);
+    let headerStyles = dayToName.map((day, index) => { return ifCurrentDay(index, 'currentDay', '') });
+    let dayToNameModified = onDayView ? [dayToName[daySelected]] : dayToName;
+    if(onDayView) {
+        headerStyles = ['currentDay'];
+    }
     return (
         <div className="row">
             <div className="col-1"></div>
             
-            {dayToName.map((day, index) => (
-                <div ref={headerContainerRef} key={index} style={{padding: '0'}} className={'col '+ifCurrentDay(index, 'currentDay', '')}>
+            {dayToNameModified.map((day, index) => (
+                <div ref={headerContainerRef} key={index} style={{padding: '0'}} className={'col '+headerStyles[index]}>
                     <span className='timeboxHeadingText'>{day.name}<br />{" ("+day.date+"/"+day.month+")"}</span>
                     {/*ifCurrentDay(index, true, false) ? (<>
                         {/*<ActiveOverlay></ActiveOverlay>
