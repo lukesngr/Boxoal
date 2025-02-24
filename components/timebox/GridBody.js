50d9d9014461c01391c4896882e20db52ed39a04
Luke Sanger <lukesngr@gmail.com>
2025-02-24 18:18:13 +1100

    removing day view feature, looks too goofy on normal sccreen

--- components/timebox/GridBody.js
+++ components/timebox/GridBody.js
@@ -1,14 +1,10 @@
-import { useSelector } from "react-redux";
 export function GridBody({listOfTimes, dayToName, timeboxColumnRef}) {
-    const onDayView = useSelector(state => state.onDayView.value);
-    const daySelected = useSelector(state => state.daySelected.value);
-    let dayToNameModified = onDayView ? [dayToName[daySelected]] : dayToName;
     return (
         <>
             {listOfTimes.map(time => (
                 <div key={time} className="row">
                     <div ref={timeboxColumnRef} className="col-1 timeCol">{time}</div>
-                    {dayToNameModified.map((day, index) => (
+                    {dayToName.map((day, index) => (
                         <></>
                     ))}
                 </div>))
--- components/timebox/GridHeader.js
+++ components/timebox/GridHeader.js
@@ -2,29 +2,15 @@ import Overlay from '../overlay/Overlay';
 import ActiveOverlay from '../overlay/ActiveOverlay';
 import RecordingOverlay from '../overlay/RecordingOverlay';
 import RecordedTimeBoxOverlay from './RecordedTimeBoxOverlay';
-import '../../styles/timeboxes.scss';
-
-import { useSelector } from 'react-redux';
 import { ifCurrentDay, ifEqualOrBeyondCurrentDay } from '../../modules/untestableFunctions';
 export default function GridHeader({headerContainerRef, dayToName}) {
-    const onDayView = useSelector(state => state.onDayView.value);
-    const daySelected = useSelector(state => state.daySelected.value);
-    let headerStyles = dayToName.map((day, index) => { return ifCurrentDay(index, 'currentDay', '') });
-    let dayToNameModified = onDayView ? [dayToName[daySelected]] : dayToName;
-    if(onDayView) {
-        headerStyles = ['currentDay onDayViewHeader'];
-    }
     return (
         <div className="row">
             <div className="col-1"></div>
             
-            {dayToNameModified.map((day, index) => (
-                <div ref={headerContainerRef} key={index} style={{padding: '0'}} className={'col '+headerStyles[index]}>
-                    <span className='timeboxHeadingText'>
-                        {day.name}
-                        {!onDayView && <br />}
-                        {" ("+day.date+"/"+day.month+")"}
-                    </span>
+            {dayToName.map((day, index) => (
+                <div ref={headerContainerRef} key={index} style={{padding: '0'}} className={'col '+ifCurrentDay(index, 'currentDay', '')}>
+                    <span className='timeboxHeadingText'>{day.name}<br />{" ("+day.date+"/"+day.month+")"}</span>
                     {/*ifCurrentDay(index, true, false) ? (<>
                         {/*<ActiveOverlay></ActiveOverlay>
                         <RecordingOverlay></RecordingOverlay>
