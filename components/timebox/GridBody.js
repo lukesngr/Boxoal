ba74d7ad72c0e54d520079f6481b0bff4d4ace14
Luke Sanger <lukesngr@gmail.com>
2025-02-24 17:45:53 +1100

    added day view as can with this code available

--- components/timebox/GridBody.js
+++ components/timebox/GridBody.js
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
 export function GridBody({listOfTimes, dayToName, timeboxColumnRef}) {
+    const onDayView = useSelector(state => state.onDayView.value);
+    const daySelected = useSelector(state => state.daySelected.value);
+    let dayToNameModified = onDayView ? [dayToName[daySelected]] : dayToName;
     return (
         <>
             {listOfTimes.map(time => (
                 <div key={time} className="row">
                     <div ref={timeboxColumnRef} className="col-1 timeCol">{time}</div>
-                    {dayToName.map((day, index) => (
+                    {dayToNameModified.map((day, index) => (
                         <></>
                     ))}
                 </div>))
