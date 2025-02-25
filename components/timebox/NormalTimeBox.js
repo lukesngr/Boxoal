fab157d8d6f37d3136672dcf4daa70c7d25e294b
Luke Sanger <lukesngr@gmail.com>
2025-02-25 19:38:47 +1100

    remove console.log as this is working

--- components/timebox/NormalTimeBox.js
+++ components/timebox/NormalTimeBox.js
@@ -7,10 +7,9 @@ export default function NormalTimeBox(props) {
     const {boxSizeNumber, boxSizeUnit} = useSelector(state => state.profile.value);
     const [timeboxActionsFormVisible, setTimeboxActionsFormVisible] = useState(false);
     let percentageOfBoxSizeFilled = getPercentageOfBoxSizeFilled(boxSizeUnit, boxSizeNumber, new Date(props.data.startTime), new Date(props.data.endTime));
-    console.log(percentageOfBoxSizeFilled);
     let calculatedHeight = `calc(${(Number(percentageOfBoxSizeFilled*100))}% + ${Number((percentageOfBoxSizeFilled-1)*2)}px)`
     return (<>
-        <TimeboxActionsForm visible={timeboxActionsFormVisible} data={props.data} date={props.date} time={props.time}></TimeboxActionsForm>
+        <TimeboxActionsForm visible={timeboxActionsFormVisible} closeModal={() => setTimeboxActionsFormVisible(false)} data={props.data} date={props.date} time={props.time}></TimeboxActionsForm>
         <div style={{height: '100%'}} onClick={() => setTimeboxActionsFormVisible(true)}>
             <div style={{height: `${calculatedHeight}`, backgroundColor: props.data.color, zIndex: 999, position: 'relative'}} id="timeBox" data-testid="normalTimeBox">    
                 <span className="timeboxText">{props.data.title}</span>
