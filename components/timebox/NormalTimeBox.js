5aec4b5027e66632a4b68fc5b154521417f754a4
Luke Sanger <lukesngr@gmail.com>
2025-02-25 18:34:09 +1100

    working perfectly now, time to add timebox actions form

--- components/timebox/NormalTimeBox.js
+++ components/timebox/NormalTimeBox.js
@@ -5,9 +5,9 @@ export default function NormalTimeBox(props) {
     const {boxSizeNumber, boxSizeUnit} = useSelector(state => state.profile.value);
     let percentageOfBoxSizeFilled = getPercentageOfBoxSizeFilled(boxSizeUnit, boxSizeNumber, new Date(props.data.startTime), new Date(props.data.endTime));
     console.log(percentageOfBoxSizeFilled);
-    let height = Number(percentageOfBoxSizeFilled*100);
+    let calculatedHeight = `calc(${(Number(percentageOfBoxSizeFilled*100))}% + ${Number((percentageOfBoxSizeFilled-1)*2)}px)`
     return (
-        <div style={{height: `${height}%`, backgroundColor: props.data.color}} id="timeBox" data-testid="normalTimeBox">    
+        <div style={{height: `${calculatedHeight}`, backgroundColor: props.data.color, zIndex: 999, position: 'relative'}} id="timeBox" data-testid="normalTimeBox">    
             <span className="timeboxText">{props.data.title}</span>
         </div>
     )
