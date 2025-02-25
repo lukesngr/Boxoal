915c9672a1e696323adbcca01b0649b05bf50121
Luke Sanger <lukesngr@gmail.com>
2025-02-25 19:25:24 +1100

    added state and modal to normal timebox but it is not visible

--- components/timebox/NormalTimeBox.js
+++ components/timebox/NormalTimeBox.js
@@ -1,14 +1,19 @@
 import { useSelector } from "react-redux";
 import { getPercentageOfBoxSizeFilled } from "@/modules/boxCalculations";
+import TimeboxActionsForm from "../form/TimeboxActionsForm";
 
 export default function NormalTimeBox(props) {
     const {boxSizeNumber, boxSizeUnit} = useSelector(state => state.profile.value);
+    const [timeboxActionsFormVisible, setTimeboxActionsFormVisible] = useState(false);
     let percentageOfBoxSizeFilled = getPercentageOfBoxSizeFilled(boxSizeUnit, boxSizeNumber, new Date(props.data.startTime), new Date(props.data.endTime));
     console.log(percentageOfBoxSizeFilled);
     let calculatedHeight = `calc(${(Number(percentageOfBoxSizeFilled*100))}% + ${Number((percentageOfBoxSizeFilled-1)*2)}px)`
-    return (
-        <div style={{height: `${calculatedHeight}`, backgroundColor: props.data.color, zIndex: 999, position: 'relative'}} id="timeBox" data-testid="normalTimeBox">    
-            <span className="timeboxText">{props.data.title}</span>
+    return (<>
+        <TimeboxActionsForm visible={timeboxActionsFormVisible} data={props.data} date={props.date} time={props.time}></TimeboxActionsForm>
+        <div style={{height: '100%'}} onClick={setTimeboxActionsFormVisible(true)}>
+            <div style={{height: `${calculatedHeight}`, backgroundColor: props.data.color, zIndex: 999, position: 'relative'}} id="timeBox" data-testid="normalTimeBox">    
+                <span className="timeboxText">{props.data.title}</span>
+            </div>
         </div>
-    )
+    </>)
 }
