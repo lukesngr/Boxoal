01cee68e92583d48e849c3ae2d8f06d996d87993
Luke Sanger <lukesngr@gmail.com>
2025-02-25 18:25:41 +1100

    appears to be working

--- components/timebox/TimeboxInCreation.js
+++ components/timebox/TimeboxInCreation.js
@@ -26,8 +26,8 @@ export default function TimeboxInCreation({date, time, day}) {
             setTitle={setTitle}>
         </CreateTimeboxForm>
         <div onClick={() => setCreateTimeboxFormVisible(true)} style={{height: '100%'}}>
-            <div className="timeboxInCreation" style={{height: getHeightForBoxes(numberOfBoxes), backgroundColor: 'blue', width: '100%', marginTop: '0px'}}>
-                <span className="timeboxText">{title}</span>
+            <div className="timeboxInCreation" style={{height: getHeightForBoxes(numberOfBoxes), backgroundColor: 'blue', width: '100%', marginTop: '0px', zIndex: 999, position: 'relative'}}>
+                <span style={{position: 'relative', fontSize: 'medium', left: '2px', top: '2px', zIndex: 998, overflow: 'hidden', color: 'black'}}>{title}</span>
             </div>
         </div>
     </>
