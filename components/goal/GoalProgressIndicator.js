1d334b58018303ef6609b4021e658d31e47778c1
Luke Sanger <lukesngr@gmail.com>
2025-03-02 13:25:32 +1100

    added class to progress indicator for use in scss

--- components/goal/GoalProgressIndicator.js
+++ components/goal/GoalProgressIndicator.js
@@ -16,7 +16,7 @@ export default function GoalProgressIndicator(props) {
 
 
     return (
-    <div style={{marginLeft: 10, marginRight: 10, display: 'inline', verticalAlign: 'top'}}>
+    <div className="goalProgressIndicator">
         <svg
             width={size}
             height={size}
