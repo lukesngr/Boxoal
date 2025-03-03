fa1ae255ed8075a48bd6759dcf144442f6aa6419
Luke Sanger <lukesngr@gmail.com>
2025-03-03 22:05:00 +1100

    making style adjustments to make timebox list item indented

--- components/timebox/TimeboxAsListItem.js
+++ components/timebox/TimeboxAsListItem.js
@@ -3,15 +3,18 @@ import { IconButton } from "@mui/material";
 import { thereIsNoRecording } from "@/modules/coreLogic";
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useState } from "react";
+import { convertToTimeAndDate } from "@/modules/formatters";
 
 export default function TimeboxAsListItem(props) {
+    console.log(props.data);
     const [editTimeboxShown, setEditTimeboxShown] = useState(false);
-    const noPreviousRecording = thereIsNoRecording(data.recordedTimeBoxes, data.reoccuring, date, time);
+    let [time, date] = convertToTimeAndDate(props.data.startTime);
+    const noPreviousRecording = thereIsNoRecording(props.data.recordedTimeBoxes, props.data.reoccuring, date, time);
     return (
         <>  
-        {editTimeboxShown && <EditTimeboxForm close={() => setEditTimeboxShown(false)} data={props.data}></EditTimeboxForm>}
-        <div className="goalAccordion" >          
-            <span className='goalTitle'>{props.data.title}</span>
+        {editTimeboxShown && <EditTimeboxForm back={() => setEditTimeboxShown(false)} data={props.data} previousRecording={noPreviousRecording}></EditTimeboxForm>}
+        <div className="timeboxListItem" >          
+            <span className='timeboxListItemTitle'>{props.data.title}</span>
             
             <IconButton style={{float: 'inline-end', padding: '5px'}} onClick={() => setEditTimeboxShown(true)}>
                 <SettingsIcon></SettingsIcon>
