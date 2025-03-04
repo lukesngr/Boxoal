c6435d720006409b8d65e769d93a458b25858b86
Luke Sanger <lukesngr@gmail.com>
2025-03-04 22:02:31 +1100

    changed code to work with no data

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,49 @@
-export default function Dashboard() {
-    return (
+import SignedInNav from "./nav/SignedInNav";
+import { useDispatch } from "react-redux";
+import { useProfile } from "../hooks/useProfile";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Dashboard({user}) {
+
+    const dispatch = useDispatch();
+    const {scheduleIndex} = useSelector(state => state.profile.value);
+    let {userId, username} = user;
+    let recordedTimeboxes = [];
+    let averageProgress = 0;
+    let goalsCompleted = 0;
+    useProfile(userId, dispatch);
+
+    const {status, data, error, refetch} = useQuery({
+        queryKey: ["schedule"], 
+        queryFn: async () => {
+            const response = await axios.get(serverIP+"/getSchedules", { params: {
+                userUUID: userId
+            }});
+            return response.data;
+        },
+        enabled: true
+    })
+
+    if(status === 'loading') return <Loading />
+    if(status === 'error') return <p>Error: {error.message}</p>
+
+    if(data.length != 0) {
+        let dataForSchedule = data[scheduleIndex]
+        goalsCompleted = dataForSchedule.goals.reduce((count, item) => item.completed ? count + 1 : count, 0);
+        
+        for(let goal of dataForSchedule.goals) {
+            if(!goal.completed) {
+            averageProgress += getProgressWithGoal(goal.timeboxes);
+            }
+        }
+        if(dataForSchedule.goals.length != 0) { averageProgress = averageProgress / dataForSchedule.goals.length; }
+        recordedTimeboxes = dataForSchedule.recordedTimeboxes;
+    }
+
+    return (<>
+        <SignedInNav username={username}></SignedInNav>
         <div>
             <h1>Dashboard</h1>
         </div>
-    )
+        </>)
 }
