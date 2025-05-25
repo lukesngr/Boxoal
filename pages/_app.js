257e25155acaa858a818018ece6cafe15d9b7da8
Luke Sanger <lukesngr@gmail.com>
2025-05-25 19:42:54 +1000

    icons added

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ import {ThemeProvider, createTheme} from "@mui/material/styles";
 import { configureAmplify } from "@/modules/awsConfig";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { queryClient } from "@/modules/queryClient";
+import Head from 'next/head'
 import { useState } from "react";
 import dayjs from "dayjs";
 var utc = require("dayjs/plugin/utc");
@@ -37,8 +38,16 @@ function BoxAlc({ Component, pageProps: { session, ...pageProps} }) {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   
-  return (
-    
+  return (<>
+    <Head>
+      <title>Boxoal</title>
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="BoxoalIcon" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
     <Authenticator.Provider>
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
@@ -51,7 +60,7 @@ function BoxAlc({ Component, pageProps: { session, ...pageProps} }) {
         </QueryClientProvider>
       </ThemeProvider>
     </Authenticator.Provider>
-  );
+    </>);
 }
 
 export default BoxAlc;
