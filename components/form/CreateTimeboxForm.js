c3c71120b9081c8359a0dd05e673d5e5a577ecff
Luke Sanger <lukesngr@gmail.com>
2025-06-18 22:45:24 +1000

    sentry working somewhat but causing issues

--- components/form/CreateTimeboxForm.js
+++ components/form/CreateTimeboxForm.js
@@ -24,6 +24,7 @@ import Collapse from '@mui/material/Collapse';
 import { muiActionButton, muiFormControlStyle, muiInputStyle, muiNonActionButton, muiToggleButtonStyle } from '@/modules/muiStyles.js';
 import Alert from '../base/Alert.js';
 import { ToggleButton, ToggleButtonGroup, Slider, Typography } from '@mui/material';
+import * as Sentry from "@sentry/nextjs";
 
 const listOfColors = ["#00E3DD", "#00C5E6", "#00A4E7", "#0081DC", "#1E5ABF", "#348D9D", "#67D6FF"]
 
@@ -80,6 +81,7 @@ export default function CreateTimeboxForm({ visible, time, date, close, numberOf
         },
         onError: (error, goalData, context) => {
             queryClient.setQueryData(['schedule'], context.previousGoals);
+            Sentry.captureException(error);
             setAlert({ open: true, title: "Error", message: "An error occurred, please try again or contact the developer" });
             queryClient.invalidateQueries(['schedule']);
             closeModal();
