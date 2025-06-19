f207be2e8c057d7dbc73822b853b2d7536a98e8b
Luke Sanger <lukesngr@gmail.com>
2025-06-19 10:09:55 +1000

    not sentry somehow i didn't pick this shit up
    This reverts commit 5316599729f908b6d8c382ca8bb426c0367f305a.

--- components/form/CreateTimeboxForm.js
+++ components/form/CreateTimeboxForm.js
@@ -24,6 +24,7 @@ import Collapse from '@mui/material/Collapse';
 import { muiActionButton, muiFormControlStyle, muiInputStyle, muiNonActionButton, muiToggleButtonStyle } from '@/modules/muiStyles.js';
 import Alert from '../base/Alert.js';
 import { ToggleButton, ToggleButtonGroup, Slider, Typography } from '@mui/material';
+import * as Sentry from "@sentry/nextjs";
 
 const listOfColors = ["#00E3DD", "#00C5E6", "#00A4E7", "#0081DC", "#1E5ABF", "#348D9D", "#67D6FF"]
 
