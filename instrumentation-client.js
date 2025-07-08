8bcbb7e375623448e6427ad2107dcea08aa32216
Luke Sanger <lukesngr@gmail.com>
2025-07-09 09:43:44 +1000

    previous unadding of sentry config cause major fcking problems

---
+++ instrumentation-client.js
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1df9254fc455ec07da7a7527c4b58147@o4509511383842816.ingest.us.sentry.io/4509511398785024",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
