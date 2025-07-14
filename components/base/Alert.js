e0a641a78cb556c33150c09ae948db49a2aa559b
Luke Sanger <lukesngr@gmail.com>
2025-07-14 19:02:56 +1000

    fixed alert structure so now can test deletes

--- components/base/Alert.js
+++ components/base/Alert.js
@@ -3,8 +3,8 @@ import { muiNonActionButton } from "@/modules/muiStyles";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-export default function Alert({alert, setAlert}) {
-    const {alert} = useSelector(state => state.alert.value);
+export default function Alert() {
+    const alert = useSelector(state => state.alert.value);
     const dispatch = useDispatch();
     return (
         <Dialog className="errorAlert" open={alert.open} onClose={() => dispatch({type:'alert/set', payload: {open: false, title: "", message: ""}})}>
