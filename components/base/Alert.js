21594adff2cb7485b205b970ac761870c91242c3
Luke Sanger <lukesngr@gmail.com>
2025-07-14 18:40:45 +1000

    alert now based on redux time to fix code across app

--- components/base/Alert.js
+++ components/base/Alert.js
@@ -1,17 +1,20 @@
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material";
 import { muiNonActionButton } from "@/modules/muiStyles";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Alert({alert, setAlert}) {
+    const {alert} = useSelector(state => state.alert.value);
+    const dispatch = useDispatch();
     return (
-        <Dialog className="errorAlert" open={alert.open} onClose={() => setAlert({open: false, title: "", message: ""})}>
+        <Dialog className="errorAlert" open={alert.open} onClose={() => dispatch({type:'alert/set', payload: {open: false, title: "", message: ""}})}>
             <DialogTitle>{alert.title}</DialogTitle>
             <DialogContent>
                 <DialogContentText>
                 {alert.message}
                 </DialogContentText>
                 <DialogActions>
-                    <Button className="errorCloseButton" onClick={() => setAlert({open: false, title: "", message: ""})} sx={muiNonActionButton}>Close</Button>
+                    <Button className="errorCloseButton" onClick={() => dispatch({type:'alert/set', payload: {open: false, title: "", message: ""}})} sx={muiNonActionButton}>Close</Button>
                 </DialogActions>
             </DialogContent>
         </Dialog>
