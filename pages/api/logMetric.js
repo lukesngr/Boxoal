29c47d47d995b0d1ffd7aad459b7613b4d27e6f1
Luke Sanger <lukesngr@gmail.com>
2025-07-21 22:44:14 +1000

    added create log

---
+++ pages/api/logMetric.js
@@ -0,0 +1,23 @@
+import prisma from "@/modules/prismaClient";
+import * as Sentry from "@sentry/nextjs";
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const data = req.body;
+    await prisma.loggingOfMetric.create({
+      data: data,
+    });
+
+    res.status(200).json({ message: 'Schedule created successfully' });
+  } catch (error) {
+    Sentry.captureException(error);
+
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
