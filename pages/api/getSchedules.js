29afc1008b55c2f0a3ffeb3c18f641e4a8d242ef
Luke Sanger <lukesngr@gmail.com>
2025-07-27 22:32:26 +1000

    removed metric unit from here

--- pages/api/getSchedules.js
+++ pages/api/getSchedules.js
@@ -32,7 +32,6 @@ export default async function handler(req, res) {
             partOfLine: true,
             objectUUID: true,
             metric: true,
-            metricUnit: true,
             loggingsOfMetric: {
               select: {
                 date: true,
