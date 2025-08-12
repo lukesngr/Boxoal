aa38fa45e1bc1fc832734d8256595fef64daf69a
Luke Sanger <lukesngr@gmail.com>
2025-08-13 09:23:49 +1000

    this should be working now

--- pages/api/getGoals.js
+++ pages/api/getGoals.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         title: true,
         goals: {
           orderBy: {
-            partOfLine: 'asc'
+            targetDate: 'desc'
           },
           select: {
             id: true,
