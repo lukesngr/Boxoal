787d2bcc91fe501e21569cfe26a5793a2d4d13d2
Luke Sanger <lukesngr@gmail.com>
2025-08-13 21:30:37 +1000

    color is red when failed

--- components/goaltree/GoalTreeView.js
+++ components/goaltree/GoalTreeView.js
@@ -62,8 +62,8 @@ export default function GoalTreeView(props) {
                         <span className="goalCardTitle">{goal.title}</span>
                         <span className="goalCardUndertext">{dayjs(goal.targetDate).format('D MMM')}</span>
                         {goal.state == "completed" && <span style={{color: '#4FF38E'}} className="goalCardUndertext">Completed</span>}
-                        {goal.state == "failed" && <span className="goalCardUndertext">Failed</span>}
-                        {goal.state == "active" && <span className="goalCardUndertext">{goal.percentageCompleted}%</span>}
+                        {goal.state == "failed" && <span style={{color: '#FF0606'}} className="goalCardUndertext">Failed</span>}
+                        {goal.state == "active" && <span  className="goalCardUndertext">{goal.percentageCompleted}%</span>}
                     </div>
                     <svg width={50} height={45} viewBox="0 0 24 30">
                         <path 
