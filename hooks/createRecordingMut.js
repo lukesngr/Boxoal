4f48cf91546896cb3b191a9d5e0716d15d4bfcff
Luke Sanger <lukesngr@gmail.com>
2026-01-04 19:25:30 +1100

    abstraction of mutation into seperate file means i can fix recording of reoccurring terminating specific reoccurring timebox

---
+++ hooks/createRecordingMut.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { useMutation } from "react-query";
+import { queryClient } from '../modules/queryClient.js';
+
+export default function createRecordingMut(timeboxData, close, dispatch) { 
+
+  return useMutation({
+    mutationFn: (recordingData) => axios.post('/api/createRecordedTimebox', recordingData),
+    onMutate: async (recordingData) => {
+      await queryClient.cancelQueries(['schedule']); 
+            
+      const previousSchedule = queryClient.getQueryData(['schedule']);
+            
+      queryClient.setQueryData(['schedule'], (old) => {
+        if (!old) return old;
+        //recordedTimeBoxes in schedule
+        const copyOfOld = structuredClone(old);
+        const recordingDataCopy = structuredClone(recordingData);
+        recordingDataCopy.timeBox = timeboxData
+        copyOfOld[scheduleIndex].recordedTimeboxes.push(recordingDataCopy);
+
+        //recordedTimeboxes in timeboxes
+        const timeboxIndex = copyOfOld[scheduleIndex].timeboxes.findIndex(element => element.objectUUID == timeboxData.objectUUID);
+        copyOfOld[scheduleIndex].timeboxes[timeboxIndex].recordedTimeBoxes.push(recordingDataCopy);
+
+        //recordedTimeBoxes in goals
+        const goalIndex = copyOfOld[scheduleIndex].goals.findIndex(element => element.id == Number(timeboxData.goalID));
+        const timeboxGoalIndex = copyOfOld[scheduleIndex].goals[goalIndex].timeboxes.findIndex(element => element.objectUUID == timeboxData.objectUUID);
+                
+        copyOfOld[scheduleIndex].goals[goalIndex].timeboxes[timeboxGoalIndex].recordedTimeBoxes.push(recordingDataCopy);
+        return copyOfOld;
+       }); 
+            return { previousSchedule };
+    },
+    onSuccess: () => {
+      close();
+      dispatch({type: 'alert/set', payload: {
+        open: true,
+        title: "Timebox",
+        message: "Added recorded timebox!"
+      }});
+        queryClient.invalidateQueries(['schedule']); // Refetch to get real data
+      },
+    onError: (error, goalData, context) => {
+      queryClient.setQueryData(['schedule'], context.previousGoals);
+      dispatch({type: 'alert/set', payload: { 
+	open: true, 
+	title: "Error", 
+	message: "An error occurred, please try again or contact the developer" 
+      }});
+      queryClient.invalidateQueries(['schedule']);
+      close();
+    }
+  });
+}
