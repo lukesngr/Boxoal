6e5b6aa1b19ea21d850e1bc637fe71fb03225874
Luke Sanger <lukesngr@gmail.com>
2026-05-02 11:20:23 +1000

    added more fixes to Overlay

--- components/overlay/Overlay.js
+++ components/overlay/Overlay.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 export default function Overlay(props) {
     const overlayDimensions = useSelector(state => state.overlayDimensions.value);
-    
+        
     return (<>{!props.active && <div className="overlay" style={{width: overlayDimensions.headerWidth+"px", 
         height: overlayDimensions.overlayHeight+"px"}}></div>}</>);
 }
